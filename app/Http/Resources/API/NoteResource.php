8127cd09329149f9d4d7bbbaab4a22d9370afc4e
ilhammmaulana <ilhammmaulana688@gmail.com>
2023-06-08 16:13:23 +0700

    category_name

--- app/Http/Resources/API/NoteResource.php
+++ app/Http/Resources/API/NoteResource.php
@@ -23,7 +23,6 @@ class NoteResource extends JsonResource
             "id" => $this->id,
             "title" => $this->title,
             "body" => $this->body,
-            "category_id" => $this->category,
             "category" => $this->category_id === null ? null : [
                 "id" => $this->category_id,
                 "category_name" => $this->category->category_name,
